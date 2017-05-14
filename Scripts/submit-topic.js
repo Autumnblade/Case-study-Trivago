8e7601f2b07be2247178294b403ff95e31ef553b
DESKTOP-OR9F8R2\Roald <Roaldteunissen@gmail.com>
2017-05-14 20:19:04 +0200

    Changed form submitting and added review panels - Form serializes to an array - Topic is saved in a variable - Added a function to create review panels(visually) - Gets the first json file and converts it to panels to show visually - Fixed a style problem(form was stuck in the heading of the next review)

--- Scripts/submit-topic.js
+++ Scripts/submit-topic.js
@@ -1,10 +1,67 @@
 // Use Jquery document ready for browser compatibility 
 $(document).ready( function() {
-    console.log('Document ready!');
-    
-    var submitButton = document.getElementById('submitTopic');
+    var form = $('#ajax-topic');
 
-    submitButton.addEventListener('click', function() {
-        console.log('Submitting the topic..');
+    $(form).on("submit", function (event) {
+        // Prevents the browser from submitting the form
+        event.preventDefault();
+
+        // Serializes the form data to an array.
+        var formData = $(this).serializeArray();
+
+        // Declare essential variable
+        var topic = null;
+
+        // Iterates through with as purpose to initialize the essential variable
+        $.each(formData, function (i, obj){
+            if (obj.name == 'topic') {
+                topic = obj.value;
+            }
+        });
+        
+        // Request for the review
+        $.getJSON('../Reviews/reviews1.json', function (data) {
+            var reviews = data["Reviews"];
+
+            // Creates the reviews visually
+            $.each(reviews, function(i, val) {
+                createReviewPanel(val);
+            });
+        });
     });
 });
+
+function createReviewPanel(review) {
+    var content = $('#content');
+
+    var panel = $('<div class="panel panel-default"></div>');
+
+    // Header
+    var pHeading = $('<div class="panel-heading"></div>');
+
+    var title = $('<h3 class="panel-title"></h3>').text(review['Author']);
+
+    pHeading.append(title);
+    
+    // End header
+    // Body
+    var pBody = $('<div class="panel-body row"></div>');
+
+    var pRatings = $('<div class="col-md-3"></div>');
+    var pContent = $('<div class="col-md-9"></div>');
+
+    var contentText = review['Content'];
+    pContent.append(contentText);
+
+    // End Body
+    // Append the sections to the panel's body
+    pBody.append(pRatings);
+    pBody.append(pContent);
+
+    // Puts the panel together
+    panel.append(pHeading);
+    panel.append(pBody);
+
+    // Finish with putting the panel in the content area
+    content.append(panel);
+}
