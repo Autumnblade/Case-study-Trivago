7a9202a08f23fb7c79401944d003b0d33febecc6
DESKTOP-OR9F8R2\Roald <Roaldteunissen@gmail.com>
2017-05-20 15:32:23 +0200

    Hotel Panel - Added a function for creating a hotel panel -- Added image, address, name, price and image link. - Fixes that all the reviews were multiplied(clearing the area for the next search) - Made some name givings more logical

--- Scripts/submit-topic.js
+++ Scripts/submit-topic.js
@@ -21,7 +21,14 @@ $(document).ready( function() {
         
         // Request for the review
         $.getJSON('../Reviews/reviews1.json', function (data) {
+            var hotel = data["HotelInfo"];
             var reviews = data["Reviews"];
+
+            // Clears the area for the next search
+            $("#content").empty();
+
+            // Creates the hotel visually 
+            createHotelPanel(hotel);
 
             // Creates the reviews visually
             $.each(reviews, function(i, val) {
@@ -30,6 +37,49 @@ $(document).ready( function() {
         });
     });
 });
+
+function createHotelPanel(hotel){
+    var content = $('#content');
+
+    var panel = $('<div class="panel panel-info"></div>');
+
+    // Header
+    var pHeading = $('<div class="panel-heading"></div>');
+
+    var title = $('<h3 class="panel-title"></h3>').text(hotel['Name']);
+
+    pHeading.append(title);
+    
+    // End Header / Begin Body
+    var pBody = $('<div class="panel-body row"></div>');
+
+    /// left(image) side
+    var leftSection = $('<div class="col-md-3"></div>');
+    var image = $('<img class="img-thumbnail" src=' + hotel['ImgURL'] + '>');
+    var imageLink = $('<a href="http://www.tripadvisor.com' + hotel['HotelURL'] + '"></a>');
+    imageLink.append(image);
+        
+    /// right(content) side
+    var rightSection = $('<div class="col-md-9"></div>');
+    var price = $('<h3></h3>').text(hotel['Price']);
+    var rsText = hotel['Address'];
+
+    leftSection.append(imageLink);
+    rightSection.append(price);
+    rightSection.append(rsText);
+
+    // End Body 
+    // Append the sections to the panel's body
+    pBody.append(leftSection);
+    pBody.append(rightSection);
+
+    // Puts the panel together
+    panel.append(pHeading);
+    panel.append(pBody);
+
+    // Finish with putting the panel in the content area
+    content.append(panel);
+}
 
 function createReviewPanel(review) {
     var content = $('#content');
@@ -49,19 +99,21 @@ function createReviewPanel(review) {
     // Body
     var pBody = $('<div class="panel-body row"></div>');
 
-    var pRatings = $('<div class="col-md-3"></div>');
-    var pContent = $('<div class="col-md-9"></div>');
-    
+    /// left(ratings) side
+    var leftSection = $('<div class="col-md-3"></div>');
     var ratingList = ratings(review['Ratings']);
-    pRatings.append(ratingList);
 
+    /// right(content) side
+    var rightSection = $('<div class="col-md-9"></div>');
     var contentText = review['Content'];
-    pContent.append(contentText);
+
+    leftSection.append(ratingList);
+    rightSection.append(contentText);
 
     // End Body
     // Append the sections to the panel's body
-    pBody.append(pRatings);
-    pBody.append(pContent);
+    pBody.append(leftSection);
+    pBody.append(rightSection);
 
     // Puts the panel together
     panel.append(pHeading);
